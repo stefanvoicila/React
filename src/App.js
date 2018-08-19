d7c6619d2739d36bb81791d33b9831c44bf5b0f7
Stefan <voicilastefan97@gmail.com>
2018-08-19 23:43:56 +0300

    error handling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
-import Person from './Person/Person.js';
+import classes from './App.css';
+import Person from './Person/Person';
+import ErrorBoundary from './ErrorBoundary/ErrorBoundary';
 
 class App extends Component {
   state = {
@@ -46,50 +47,42 @@ togglePersonsHandler = () => {
 
 
   render() {
-
-    const style = {
-      backgroundColor: 'green',
-      color: 'white',
-      font: 'inherit',
-      border: '1p x solid blue',
-      padding: '8px',
-      cursor: 'pointer',
-    };
-
     let persons = null;
+    let btnClass = '';
 
     if(this.state.showPersons) {
       persons = (
         <div>
           {this.state.persons.map((person, index) => {
-            return <Person
-              click = {() => this.deletePersonHandler(index)}
-              name = {person.name}
-              age = {person.age} 
-              key = {person.id}
-              changed = {(event) =>this.nameChangedHandler(event, person.id)} />
+            return <ErrorBoundary key = {person.id}>
+              <Person
+                click = {() => this.deletePersonHandler(index)}
+                name = {person.name}
+                age = {person.age} 
+                changed = {(event) =>this.nameChangedHandler(event, person.id)} />
+            </ErrorBoundary>
           })}
         </div>
       );
 
-      style.backgroundColor = 'red';
+      btnClass = classes.red;
     }
 
-    const classes = [];
+    const assignedClasses = [];
     if(this.state.persons.length <= 2) {
-      classes.push('red');
+      assignedClasses.push(classes.red);
     }
     if(this.state.persons.length <= 1) {
-      classes.push('bold');
+      assignedClasses.push(classes.bold);
     }
 
 
     return (
-      <div className="App">
+      <div className={classes.App}>
         <h1>HOLA SENOR</h1>
-        <p className={classes.join(' ')}>Wabalabadubdub</p>
+        <p className={assignedClasses.join(' ')}>Wabalabadubdub</p>
         <button
-          style = { style }
+          className={btnClass}
           onClick={ this.togglePersonsHandler }>Toggle Names</button>
           {persons}
       </div>
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import './Person.css';
+import classes from './Person.css';
 
 const person = (props) => {
 
+  const rnd = Math.random();
+
+  if(rnd> 0.7) {
+    throw new Error('Something went wrong.');
+  }
+
   return (
-    <div className= 'Person'>
+
+    <div className= {classes.Person}>
       <p onClick={props.click}>YO SOY {props.name} Y YO AM {props.age} YEARS OLD</p>
       <p>{props.children}</p>
       <input type = 'text' onChange = { props.changed } value = { props.name }/>
